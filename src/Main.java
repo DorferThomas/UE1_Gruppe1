1769282e0a8a5266fb52b95800921a934cb7e0af
Dorfer Thomas Josef <1810653831@stud.fh-kufstein.ac.at>
2018-10-11 15:20:16 +0200

    alter hinzugefügt

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         String vorname = "Thomas";
-        System.out.println("Hello " + vorname + "!");
+        int alter = 24;
+        System.out.println("Hello " + vorname + "! Du bist " + alter + " Jahre alt.");
 
     }
 }
