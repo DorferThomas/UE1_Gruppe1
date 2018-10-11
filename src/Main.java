bf35ade87584401d7d144d102815e8842af86a4a
Dorfer Thomas Josef <1810653831@stud.fh-kufstein.ac.at>
2018-10-11 15:39:47 +0200

    nachname hinzgef.

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         String vorname = "Thomas";
-        int alter = 24;
-        System.out.println("Hello " + vorname + "! Du bist " + alter + " Jahre alt.");
+        byte alter = 24;
+        String nachname = " Dorfer";
+        System.out.println("Hello " + vorname + nachname + "! Du bist " + alter + " Jahre alt.");
 
     }
 }
